865b7f1a0e3ec6e854d4db53ecd4f01ee8e4a869
anischebbii <anis.chebbi@outlook.com>
2023-04-06 13:16:46 +0100

    Week 10

--- script.js
+++ script.js
@@ -1,93 +1,85 @@
-const ROCK = "rock";
-const PAPER = "paper";
-const SCISSORS = "scissors";
-
-const gameContainer = document.getElementById("game-container");
-const rockBtn = document.getElementById("rock-btn");
-const paperBtn = document.getElementById("paper-btn");
-const scissorsBtn = document.getElementById("scissors-btn");
-
-const resultMsg = document.createElement("p");
-resultMsg.classList.add("result-msg");
-gameContainer.appendChild(resultMsg);
-
-const scoreMsg = document.createElement("p");
-scoreMsg.classList.add("score-msg");
-gameContainer.appendChild(scoreMsg);
-
-const newGameBtn = document.createElement("button");
-newGameBtn.classList.add("new-game-btn");
-newGameBtn.textContent = "New Game";
-newGameBtn.style.display = "none";
-gameContainer.appendChild(newGameBtn);
-
-let playerName = prompt("Enter your name:");
-let playerScore = 0;
-let computerScore = 0;
-let roundCount = 0;
-const maxRounds = 3;
-
-function resetGame() {
-    playerScore = 0;
-    computerScore = 0;
-    roundCount = 0;
-    resultMsg.textContent = "";
-    scoreMsg.textContent = "";
-    newGameBtn.style.display = "none";
-}
-
-function playGame(playerSelection) {
-    if (roundCount >= maxRounds) {
-        return;
+document.addEventListener("DOMContentLoaded", function () {
+    const app = document.getElementById("app");
+  
+    const predefinedItems = [
+      "Milk", "Bread", "Eggs", "Butter", "Cheese", "Cereal",
+      "Fruits", "Vegetables", "Meat", "Chicken", "Fish", "Pasta"
+    ];
+  
+    function createList() {
+      const list = document.createElement("ul");
+      list.classList.add("list-group");
+      return list;
     }
-
-    // Generate computer's selection
-    const computerSelection = computerPlay();
-
-    // Compare playerSelection and computerSelection
-    let result;
-    if (playerSelection === computerSelection) {
-        result = "Tie game!";
-    } else if (
-        (playerSelection === ROCK && computerSelection === SCISSORS) ||
-        (playerSelection === PAPER && computerSelection === ROCK) ||
-        (playerSelection === SCISSORS && computerSelection === PAPER)
-    ) {
-        playerScore++;
-        result = "You win!";
-    } else {
-        computerScore++;
-        result = "You lose!";
+  
+    function createListItem(item) {
+      const li = document.createElement("li");
+      li.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+      li.textContent = item;
+  
+      const button = document.createElement("button");
+      button.classList.add("btn", "btn-danger", "btn-sm");
+      button.textContent = "Remove";
+      button.onclick = function () {
+        li.remove();
+      };
+  
+      li.appendChild(button);
+      return li;
     }
-
-    // Display the result
-    resultMsg.textContent = `Round ${roundCount+1}: You chose ${playerSelection}, computer chose ${computerSelection}. ${result}`;
-    roundCount++;
-
-    if (roundCount === maxRounds) { // 2 out of 3 player wins or maxRounds=3 end the game and display final score and the reset (play again) button
-        displayFinalScore();
+  
+    function addItemToList(list, item) {
+      const listItem = createListItem(item);
+      list.appendChild(listItem);
     }
-}
-
-function computerPlay() {
-    const choices = [ROCK, PAPER, SCISSORS];
-    return choices[Math.floor(Math.random() * choices.length)];
-}
-
-function displayFinalScore() {
-    let winner;
-    if (playerScore > computerScore) {
-        winner = `${playerName} wins!`;
-    } else if (playerScore < computerScore) {
-        winner = "Computer wins!";
-    } else {
-        winner = "It's a tie!";
-    }
-    scoreMsg.textContent = `Final Score: ${playerName} ${playerScore} - Computer ${computerScore}. ${winner}`;
-    newGameBtn.style.display = "block";
-}
-
-rockBtn.addEventListener("click", () => playGame(ROCK));
-paperBtn.addEventListener("click", () => playGame(PAPER));
-scissorsBtn.addEventListener("click", () => playGame(SCISSORS));
-newGameBtn.addEventListener("click", resetGame);
+  
+    const form = document.createElement("form");
+    const select = document.createElement("select");
+    const option = document.createElement("option");
+    const list = createList();
+  
+    form.classList.add("mb-3");
+    select.classList.add("form-select");
+    option.textContent = "Select an item or enter manually...";
+    option.disabled = true;
+    option.selected = true;
+  
+    select.appendChild(option);
+  
+    predefinedItems.forEach(item => {
+      const option = document.createElement("option");
+      option.value = item;
+      option.textContent = item;
+      select.appendChild(option);
+    });
+  
+    const otherOption = document.createElement("option");
+    otherOption.textContent = "Other...";
+    select.appendChild(otherOption);
+  
+    select.onchange = function () {
+      if (select.value === "Other...") {
+        const input = document.createElement("input");
+        input.type = "text";
+        input.classList.add("form-control", "mt-2");
+        input.placeholder = "Enter item name";
+        form.appendChild(input);
+        input.focus();
+  
+        input.onchange = function () {
+          addItemToList(list, input.value);
+          input.value = "";
+          select.value = "";
+          form.removeChild(input);
+        };
+      } else {
+        addItemToList(list, select.value);
+        select.value = "";
+      }
+    };
+  
+    form.appendChild(select);
+    app.appendChild(form);
+    app.appendChild(list);
+  });
+  
